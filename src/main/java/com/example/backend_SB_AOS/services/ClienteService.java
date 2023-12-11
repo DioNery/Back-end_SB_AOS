bfc9d5e2223864209abafae199eae68d174d7b74
IasmimHolanda <106707727+IasmimHolanda@users.noreply.github.com>
2023-12-11 12:20:25 -0300

    Update ClienteService.java

--- src/main/java/com/example/backend_SB_AOS/services/ClienteService.java
+++ src/main/java/com/example/backend_SB_AOS/services/ClienteService.java
@@ -8,25 +8,32 @@ import com.example.backend_SB_AOS.repositories.ClienteRepository;
 import java.util.List;
 import java.util.Optional;
 
+// Classe de serviço que encapsula a lógica de negócios relacionada à entidade Cliente
 @Service
 public class ClienteService {
 
+    // Injeção de dependência do ClienteRepository
     @Autowired
     private ClienteRepository clienteRepository;
 
+    // Método para obter todos os clientes
     public List<Cliente> findAll() {
         return clienteRepository.findAll();
     }
 
+    // Método para obter um cliente pelo ID
     public Optional<Cliente> findById(Long id) {
         return clienteRepository.findById(id);
     }
 
+    // Método para salvar ou atualizar um cliente
     public Cliente save(Cliente cliente) {
         return clienteRepository.save(cliente);
     }
 
+    // Método para excluir um cliente pelo ID
     public void delete(Long id) {
         clienteRepository.deleteById(id);
     }
 }
+
