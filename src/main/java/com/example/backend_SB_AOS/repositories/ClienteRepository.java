83d30cc24843b21a086f526c5478742577d6af72
IasmimHolanda <106707727+IasmimHolanda@users.noreply.github.com>
2023-12-11 12:19:39 -0300

    Update ClienteRepository.java

--- src/main/java/com/example/backend_SB_AOS/repositories/ClienteRepository.java
+++ src/main/java/com/example/backend_SB_AOS/repositories/ClienteRepository.java
@@ -3,5 +3,6 @@ package com.example.backend_SB_AOS.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backend_SB_AOS.models.Cliente;
 
+// Interface que estende JpaRepository para realizar operações de persistência relacionadas à entidade Cliente
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
